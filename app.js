1c75b8916a084ed2c90586ded4bff6e889f040ae
Bryan Gutierrez Alvarez <bryguti.alvarez@gmail.com>
2024-05-22 01:26:18 -0400

    Set main server's configurations

---
+++ app.js
@@ -0,0 +1,26 @@
+// Add main dependencies
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+// Set environment variables
+require('dotenv').config();
+
+// Create and set the server's configs
+const app = express();
+const PORT = process.env.PORT ?? 5000;
+
+// Set the static files folder
+app.use(express.static('public'));
+
+// Set template engine
+app.use(expressLayout);
+app.set('layout', './layout/main');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello from /');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running at localhost:${PORT}`);
+})
