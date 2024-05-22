3c00b731e95ebfe800973e1f4b92812d969d3fa6
Bryan Gutierrez Alvarez <bryguti.alvarez@gmail.com>
2024-05-22 02:09:12 -0400

    Change informative comments, add views path and fix layout path

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-// Add main dependencies
+// Main dependencies
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
-// Set environment variables
+// Helper dependencies
+const path = require('node:path');
+
+// Environment variables
 require('dotenv').config();
 
-// Create and set the server's configs
+// Server's configs
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
-// Set the static files folder
+// Static files folder
 app.use(express.static('public'));
 
-// Set template engine
+// Templating engine
+app.set('views', path.join(__dirname, '/src/views'));
 app.use(expressLayout);
-app.set('layout', './layout/main');
+app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-// Set routes
+// Routes
 app.use('/', require('./src/routes/home'));
 
+// Running server
 app.listen(PORT, () => {
   console.log(`App running at localhost:${PORT}`);
 })
